679d5f024a2da5f987d51449ea9746cc268dd3d9
mikke555 <mihhail.kapustin@gmail.com>
2025-06-21 13:07:34 +0300

    init

---
+++ main.py
@@ -0,0 +1,30 @@
+import csv
+import time
+
+import click
+import requests
+from requests.exceptions import RequestException, Timeout
+from rich import print
+
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
+
+
+@click.command(context_settings=CONTEXT_SETTINGS)
+@click.option("-i", "path", default="data/urls.csv", help="Input CSV file contains urls", metavar="PATH")
+def main(path):
+    with open(path, "r") as f:
+        csv_reader = csv.reader(f, delimiter="|")
+
+        for name, url in csv_reader:
+            start_time = time.time()
+            try:
+                resp = requests.get(url, timeout=3)
+                print(f'"{name}", HTTP {resp.status_code}, time {time.time() - start_time:.2f} seconds')
+            except Timeout:
+                print(f"Skipping {url}")
+            except RequestException as e:
+                print(f'"{name}", Error: {e}')
+
+
+if __name__ == "__main__":
+    main()
---
+++ query.sql
@@ -0,0 +1,14 @@
+SELECT 
+    customer_id,
+    value_date,
+    amount,
+    SUM(amount) OVER (
+        PARTITION BY customer_id, TO_CHAR(value_date, 'YYYY-MM')
+        ORDER BY value_date
+        ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+    ) AS cumulative_sum,
+    SUM(amount) OVER (PARTITION BY customer_id) AS total_amount
+FROM 
+    transaction
+ORDER BY 
+    customer_id, value_date;
